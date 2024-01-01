1641920ce9e86b6bf133af4d93e68c5c9dd849f7
mhmed Rjb <moahmed2elfateh@gmail.com>
2024-01-01 04:11:00 +0200

    add all nos qty for all important doctypes

--- qtywatcher/overrides/stock_ledger_entry.py
+++ qtywatcher/overrides/stock_ledger_entry.py
@@ -1,14 +1,22 @@
 import frappe
 from erpnext.stock.doctype.stock_ledger_entry.stock_ledger_entry import StockLedgerEntry
 class CustomStockLedgerEntry(StockLedgerEntry):
-    # set custome field (nosquantity) equal to 5 
+
     def after_insert(self):
-        self.db_set('custom_nosquantity', 12)
-        print("after insert")
-        print(self.voucher_no)
-        print(self.voucher_detail_no)
-        #print custom_nosquantity from stocke entry 
+        # get last doc of voucher type
+        StockLedgerEntryVoucher_type = frappe.get_last_doc(self.voucher_type)
+        #check sign of Qty Change
+        positive_qty_change = self.actual_qty/abs(self.actual_qty)
 
+        dictData=StockLedgerEntryVoucher_type.as_dict(no_nulls=True, convert_dates_to_str=False)
 
+        childTableName=dictData['items'][0]['doctype']
 
+        ItemsTable = frappe.get_doc(childTableName, self.voucher_detail_no)
+        nosquantity=(ItemsTable.custom_nosquantity)*positive_qty_change
 
+        self.db_set('custom_nosquantity', nosquantity)
+
+        print("After insert:")
+        print(f"Voucher No: {self.voucher_no}")
+        print(f"Voucher Detail No: {self.voucher_detail_no}")
